d92cedae9bac025081b71aba04e8df4236c0d4a0
utsawtiwari <75722734+utsawtiwari@users.noreply.github.com>
2024-09-10 19:26:30 +0530

    Update flask-app.py

--- app/flask-app.py
+++ app/flask-app.py
@@ -1,1 +1,13 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/ip')
+def get_ip():
+    pod_ip = request.remote_addr
+    return f"Pod IP address: {pod_ip}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
